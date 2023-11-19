b4a2a8937bc87373c6dc796b99b01f572ec06eee
Katie Péloquin <chazpelo2@gmail.com>
2023-11-19 18:02:23 -0500

    Rewrite GLCaps::getDriverVersion() with glGetString(GL_VERSION)
    This makes the driver version show up on  post-2006 (aka modern) hardware.

--- source/GLG3Dcpp/GLCaps.cpp
+++ source/GLG3Dcpp/GLCaps.cpp
@@ -56,7 +56,7 @@ namespace G3D {
     GLCaps::Vendor GLCaps::computeVendor() {
         std::string s = vendor();
 
-        if (s == "ATI Technologies Inc.") {
+        if (s == "ATI Technologies Inc.") { // what about modern amd cards?
             return ATI;
         } else if (s == "NVIDIA Corporation") {
             return NVIDIA;
@@ -86,92 +86,9 @@ namespace G3D {
 #endif
 
     std::string GLCaps::getDriverVersion() {
-        if (computeVendor() == MESA) {
-            // Mesa includes the driver version in the renderer version
-            // e.g., "1.5 Mesa 6.4.2"
-
-            static std::string _glVersion = (char *) glGetString(GL_VERSION);
-            int i = _glVersion.rfind(' ');
-            if (i == std::string::npos) {
-                return "Unknown (bad MESA driver string)";
-            } else {
-                return _glVersion.substr(i + 1, _glVersion.length() - i);
-            }
-        }
-
-#ifdef G3D_WIN32
-
-        std::string driver;
-
-        // Locate the windows\system directory
-        {
-            char sysDir[1024];
-            int sysSize = GetSystemDirectory(sysDir, 1024);
-            if (sysSize == 0) {
-                return "Unknown (can't find Windows directory)";
-            }
-            driver = sysDir;
-        }
-
-        switch (computeVendor()) {
-            case ATI:
-                driver = driver + "\\ati2dvag.dll";
-                break;
-
-            case NVIDIA:
-                driver = driver + "\\nv4_disp.dll";
-                break;
-
-            default:
-                return "Unknown (Unknown vendor)";
-
-        }
-
-        char *lpdriver = const_cast<char *>(driver.c_str());
-        DWORD dummy;
-
-        int size = GetFileVersionInfoSize(lpdriver, &dummy);
-        if (size == 0) {
-            return "Unknown (Can't find driver)";
-        }
-
-        void *buffer = new uint8[size];
-
-        if (GetFileVersionInfo(lpdriver, 0, size, buffer) == 0) {
-            delete[] (uint8 *) buffer;
-            return "Unknown";
-        }
-
-        // Interpret the VS_VERSIONINFO header pseudo-struct
-        VS_VERSIONINFO *pVS = (VS_VERSIONINFO *) buffer;
-        debugAssert(!wcscmp(pVS->szKey, L"VS_VERSION_INFO"));
-
-        uint8 *pVt = (uint8 *) &pVS->szKey[wcslen(pVS->szKey) + 1];
-
-#define roundoffs(a, b, r)    (((uint8*)(b) - (uint8*)(a) + ((r) - 1)) & ~((r) - 1))
-#define roundpos(b, a, r)    (((uint8*)(a)) + roundoffs(a, b, r))
-
-        VS_FIXEDFILEINFO *pValue = (VS_FIXEDFILEINFO *) roundpos(pVt, pVS, 4);
-
-#undef roundoffs
-#undef roundpos
-
-        std::string result = "Unknown (No information)";
-
-        if (pVS->wValueLength) {
-            result = format("%d.%d.%d.%d",
-                            pValue->dwProductVersionMS >> 16,
-                            pValue->dwProductVersionMS & 0xFFFF,
-                            pValue->dwProductVersionLS >> 16,
-                            pValue->dwProductVersionLS & 0xFFFF);
-        }
-
-        delete[] (uint8 *) buffer;
-
-        return result;
-#else
-        return "Unknown";
-#endif
+        // original code would check ancient driver dlls which would just
+        // cause the driver version to show up as "Unknown" on modern systems
+        return reinterpret_cast<const char *>(glGetString(GL_VERSION));
     }
 
     void GLCaps::init() {
