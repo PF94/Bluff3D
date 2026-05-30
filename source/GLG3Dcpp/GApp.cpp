90deacc4bc4109d5bfa6e8b858a500656930c5ee
space <100931586+spaceyounoob@users.noreply.github.com>
2026-05-30 17:16:41 +0800

    Update GApp.cpp

--- source/GLG3Dcpp/GApp.cpp
+++ source/GLG3Dcpp/GApp.cpp
@@ -20,9 +20,9 @@
 #include "GLG3D/Shader.h"
 #include "GLG3D/Draw.h"
 
-#include "imgui.h"
+/** #include "imgui.h"
 #include "imgui_impl_sdl2.h"
-#include "imgui_impl_opengl2.h"
+#include "imgui_impl_opengl2.h" */ 
 
 #undef main // fix sdl_main
 
